adcd8eac0a545c32f802f92741034b7c3e080449
Fereshteh19 <127944580+Fereshteh19@users.noreply.github.com>
2023-11-22 19:04:50 +0330

    add structure of weather app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Toronto" />
+
+        <footer>
+          This project  is{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
